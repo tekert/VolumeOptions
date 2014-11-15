a8fdd1e5fc8105f42fea2872777cdb183f1f7fe1
tekert <tekert@tekert.com.ar>
2014-11-15 03:08:10 -0300

    new release

--- VolumeOptions_TS3Plugin/src/plugin.cpp
+++ VolumeOptions_TS3Plugin/src/plugin.cpp
@@ -80,7 +80,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.62";
+    return "0.7.318";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
